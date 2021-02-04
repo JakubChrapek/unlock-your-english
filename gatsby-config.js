16f3d5846844af2f6f12e4628a108129a198505c
Albert Strzyżewski <strzyzewski.albertms@gmail.com>
2021-02-04 23:23:54 +0100

    Basic plugins, omiFontLoader, reactHelemet, pluginSharp, transformerSharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,41 @@ module.exports = {
   siteMetadata: {
     title: "YourEnglish",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-omni-font-loader",
+
+      options: {
+        enableListener: true,
+        interval: 500,
+        timeout: 30000,
+        web: [
+          //examples
+          {
+            name: "Poppins",
+            file: "https://fonts.googleapis.com/css2?family=Poppins",
+          },
+          {
+            name: "Libre Baskerville",
+            file: "https://fonts.googleapis.com/css2?family=Libre+Baskerville",
+          },
+          {
+            name: "JetBrains Mono",
+            file: "https://fonts.googleapis.com/css2?family=JetBrains+Mono",
+          },
+        ],
+      },
+    },
+  ],
 };
