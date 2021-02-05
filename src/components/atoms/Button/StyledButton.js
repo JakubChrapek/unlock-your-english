0b0d8995a26de5bb6b5d9cd5bfd155026fe37cbb
Albert Strzyżewski <strzyzewski.albertms@gmail.com>
2021-02-05 12:31:52 +0100

    Styled Button component

--- src/components/atoms/Button/StyledButton.js
+++ src/components/atoms/Button/StyledButton.js
@@ -1,4 +1,15 @@
 import styled from "styled-components"
 import { motion } from "framer-motion"
 
-export const StyledButton = styled(motion.button)``
+export const StyledButton = styled(motion.button)`
+  padding: ${({ hasdeclaredpadding }) =>
+    hasdeclaredpadding ? hasdeclaredpadding : "0"};
+  margin: ${({ hasdeclaredmargin }) =>
+    hasdeclaredmargin ? hasdeclaredmargin : "0"};
+  font-family: ${({ hasdeclaredfontfamily }) =>
+    hasdeclaredfontfamily ? hasdeclaredfontfamily : "Releway, sans-serif;"};
+  color: ${({ hasdeclaredfontcolor }) =>
+    hasdeclaredfontcolor ? hasdeclaredfontcolor : "var(--black)"};
+  background-color: ${({ hasdeclaredbgcolor }) =>
+    hasdeclaredbgcolor ? hasdeclaredbgcolor : "var(--white)"};
+`
