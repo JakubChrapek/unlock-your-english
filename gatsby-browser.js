523633cbab79007aaec1f3fd472b987af9884cad
jakubchrapek <chrapekuba@gmail.com>
2021-02-24 18:26:39 +0100

    Initial page loading animation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,15 @@
 const React = require("react")
 const GlobalLayout = require("./src/components/Layouts/GlobalLayout").default
+const gsap = require("gsap").gsap
 
 exports.wrapPageElement = ({ element, props }) => {
   return <GlobalLayout {...props}>{element}</GlobalLayout>
+}
+
+exports.onClientEntry = () => {
+  window.addEventListener("load", () => {
+    const body = document.querySelector("body")
+    body.className = document.body.className.replace(/\bno-js\b/, "")
+    gsap.from(body, { opacity: 0, duration: 1, ease: "Power3.easeInOut" })
+  })
 }
--- gatsby-config.js
+++ gatsby-config.js
@@ -65,18 +65,6 @@ module.exports = {
         // If you are working on development/staging environment, you might want to
         // preview the latest version of records instead of the published one:
         previewMode: false,
-
-        // Disable automatic reloading of content when some change occurs on DatoCMS:
-        disableLiveReload: false,
-
-        // Custom API base URL (most don't need this)
-        // apiUrl: 'https://site-api.datocms.com',
-
-        // Setup locale fallbacks
-        // In this example, if some field value is missing in Italian, fall back to English
-        localeFallbacks: {
-          it: ["en"],
-        },
       },
     },
     {
